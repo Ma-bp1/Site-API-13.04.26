6009734101a5f98f7e34b50be1da7757aff62110
Mari <marietec.lima244@gmail.com>
2026-04-14 22:12:39 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+const container = document.getElementById('cardsContainer');
+const selectAllCards = document.querySelector('.cards-container')
+
+//checa se cards foram carregados, se sim (true), mostrar (grid), se não, esconder (none).
+function showCards(status) {
+    isLoaded = status;
+
+    if(isLoaded) {
+        selectAllCards.style.display = 'grid';
+    }
+    else {
+        selectAllCards.style.display = 'none';
+    }
+}
+
+async function carregarGatos() {
+        container.innerHTML = "<p>Carregando...</p>";
+ 
+        try{
+            const resposta = await fetch("https://api.thecatapi.com/v1/images/search?limit=10");
+            const gatoData = await resposta.json();
+            container.innerHTML=" ";
+ 
+            gatoData.forEach(imagem => {
+                const card = document.createElement("div");
+                card.classList.add("card");
+ 
+                card.innerHTML=`
+                    <img src="${imagem.url}">
+                `
+                container.appendChild(card);      
+            });
+
+            showCards(true);
+
+        } catch(error){
+            container.innerHTML='<p>Erro ao carregar os dados dos gatos :(</p>';
+            console.error(error);
+        }
+    }
+
+document.addEventListener("DOMContentLoaded", ()=>{carregarGatos()});
